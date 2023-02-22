d432e28067b9d63cba7689db36ad25e60300e8d1
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2023-02-22 17:36:38 +0300

    3. Взаимодействие с объектами. Следование за объектами.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{ 
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    public bool isFocus = false;
+    Transform player;
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        // Этот метод будет перезаписываться, то есть когда мы
+        // будем менять или запоминать разные объекты, которых касаемся
+        Debug.Log("Interacting with" + transform.name);
+    }
+
+    void Update()
+    {
+        if(isFocus)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if(distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+}
